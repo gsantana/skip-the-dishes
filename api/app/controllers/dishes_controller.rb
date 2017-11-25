fb35183c46155930ce18e7d70bb1481b208c1f94
Eric Camalionte <eric80hz@yahoo.com.br>
2017-11-25 11:55:10 -0200

    Add a create method in DishesController to rebuild rest params

--- api/app/controllers/dishes_controller.rb
+++ api/app/controllers/dishes_controller.rb
@@ -1,2 +1,23 @@
 class DishesController < ApplicationController
+  def create
+    rebuild_params
+    super
+  end
+
+  private
+
+  def rebuild_params
+    params[:data][:relationships] = build_restaurant_relationship
+    params.require(:data)
+          .require(:relationships)
+          .require(:restaurant)
+          .require(:data)
+          .permit(:id)
+  end
+
+  def build_restaurant_relationship
+    {
+      restaurant: { data: { type: 'restaurants', id: params[:restaurant_id] } }
+    }
+  end
 end
--- api/spec/controllers/dishes_controller_spec.rb
+++ api/spec/controllers/dishes_controller_spec.rb
@@ -17,8 +17,7 @@ end
 
 def build_params
   { data: { type: 'dishes',
-            attributes: build_attributes,
-            relationships: build_relations } }
+            attributes: build_attributes} }
 end
 
 def build_attributes
@@ -26,11 +25,5 @@ def build_attributes
     name: 'Rice and Beans',
     price: 10.00,
     calories: 100
-  }
-end
-
-def build_relations
-  {
-    restaurant: { data: { type: 'restaurants', id: restaurant.id } }
   }
 end
