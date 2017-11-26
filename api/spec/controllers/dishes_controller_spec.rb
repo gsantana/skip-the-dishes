ad71e38d300b66c66dcefc62e574226844408808
Eric Camalionte <eric80hz@yahoo.com.br>
2017-11-25 22:22:44 -0200

    Add more specs for restaurants and dishes

--- api/spec/controllers/dishes_controller_spec.rb
+++ api/spec/controllers/dishes_controller_spec.rb
@@ -2,8 +2,7 @@ require "rails_helper"
 
 RSpec.describe DishesController, type: :request do
   describe 'POST' do
-    let(:valid_params) { build_params.to_json }
-
+    let(:valid_params) { build_dishes_params }
     let(:restaurant) { create :restaurant }
 
     it do
@@ -13,14 +12,54 @@ RSpec.describe DishesController, type: :request do
       expect(json['data']['id']).not_to be_nil
     end
   end
+
+  describe 'GET /restaurants/id/dishes' do
+    let(:restaurant) { create :restaurant }
+    let(:dish1) { create :dish, restaurant: restaurant }
+    let(:dish2) { create :dish, restaurant: restaurant }
+    let(:dish3) { create :dish, restaurant: restaurant }
+
+    before do
+      restaurant.dishes = [dish1, dish2, dish3]
+    end
+
+    it do
+      id = restaurant.id
+      get "/restaurants/#{id}/dishes"
+      expect(response).to have_http_status(:ok)
+      expect(json['data'].size).to be_eql 3
+    end
+  end
+
+  describe 'DELETE /restaurants/id/dishes/dish_id' do
+    let(:restaurant) { create :restaurant }
+    let(:dish) { create :dish, restaurant: restaurant }
+
+    before do
+      restaurant.dishes = [ dish ]
+    end
+
+    it do
+      id = restaurant.id
+      dish_id = dish.id
+      delete "/restaurants/#{id}/dishes/#{dish_id}"
+      expect(response).to have_http_status(:no_content)
+      restaurant.reload
+      expect(restaurant.dishes).to be_empty
+    end
+  end
 end
 
-def build_params
-  { data: { type: 'dishes',
-            attributes: build_attributes} }
+def build_dishes_params
+  {
+    data: {
+      type: 'dishes',
+      attributes: build_dishes_attributes
+    }
+  }.to_json
 end
 
-def build_attributes
+def build_dishes_attributes
   {
     name: 'Rice and Beans',
     price: 10.00,
--- api/spec/controllers/restaurant_controller_spec.rb
+++ api/spec/controllers/restaurant_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe RestaurantsController, type: :request do
-  describe 'POST' do
-    let(:valid_params) { build_params.to_json }
+  describe 'POST /restaurants' do
+    let(:valid_params) { build_restaurant_params.to_json }
 
     it do
       post '/restaurants', params: valid_params, headers: jsonapi_headers
@@ -10,13 +10,38 @@ RSpec.describe RestaurantsController, type: :request do
       expect(json['data']['id']).not_to be_nil
     end
   end
+
+  describe 'GET /restaurants' do
+    before do
+      create :restaurant
+      create :restaurant
+      create :restaurant
+    end
+
+    it do
+      get '/restaurants'
+      expect(response).to have_http_status(:ok)
+      expect(json['data'].size).to be_eql 3
+    end
+  end
+
+  describe 'DELETE /restaurant/id' do
+    let(:restaurant) { create :restaurant }
+
+    it do
+      id = restaurant.id
+      delete "/restaurants/#{id}"
+      expect(response).to have_http_status(:no_content)
+      expect(Restaurant.count).to be_eql 0
+    end
+  end
 end
 
-def build_params
-  { data: { type: 'restaurants', attributes: build_attributes } }
+def build_restaurant_params
+  { data: { type: 'restaurants', attributes: build_restaurant_attributes } }
 end
 
-def build_attributes
+def build_restaurant_attributes
   {
     name: 'Good Food',
     contact_name: 'Bill Gates',
